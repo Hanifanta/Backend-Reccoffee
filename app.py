9bb5293bedebb5aff88709a8b57eaf062590bfa7
Hanif Al Irsyad <90784532+Hanifanta@users.noreply.github.com>
2022-11-23 13:18:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def app_recom(label, similarity_data=cosine_sim_df, items=recom[['name', 'origin
     return pd.DataFrame(closest).merge(items).head(k)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
